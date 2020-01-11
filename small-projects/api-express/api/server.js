8569babeb624cc4cac5cf7e3a6bd1a0c3e8f1579
Markus Unger <schaulustig23@gmail.com>
2020-01-11 12:04:09 +0100

    basic jwt authentication and authorization

--- small-projects/api-express/api/server.js
+++ small-projects/api-express/api/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
@@ -27,6 +28,37 @@ mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${proc
   .catch((error) => {
     console.log(`Error connecting to database. ${error}`);
   });
+
+const authJWT = (req, res, next) => {
+  // check for valid JWT
+  if (!req.headers.authorization) {
+    req.isAuthed = false;
+    return next();
+  }
+
+  const token = req.headers.authorization.split(/\s+/).pop().replace(/"/g, '');
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) req.isAuthed = false;
+    if (decoded) req.isAuthed = true;
+    return next();
+  });
+};
+
+app.get('/', authJWT, (req, res) => {
+  if (req.isAuthed) res.status(200).json({ ok: 'no token necessary' });
+  jwt.sign({ authed: true }, process.env.JWT_SECRET, { algorithm: 'HS256' }, (error, token) => {
+    if (error) res.status(500).json({ error });
+    res.status(200).json({ ok: 'token sent', token });
+  });
+});
+
+app.get('/authed', authJWT, (req, res) => {
+  if (!req.isAuthed) {
+    res.status(401).json({ error: 'token not found or not validated' });
+  } else {
+    res.status(200).json({ ok: 'authed and permitted to access' });
+  }
+});
 
 
 module.exports = () => {
